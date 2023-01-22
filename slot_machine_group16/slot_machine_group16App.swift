0ea294a42f78737974239482478374c5da487586
DEVOPS.YXiong@crucialinsights.net <DEVOPS.YXiong@crucialinsights.net>
2023-01-22 13:34:55 -0500

    init project

---
+++ slot_machine_group16/slot_machine_group16App.swift
@@ -0,0 +1,17 @@
+//
+//  slot_machine_group16App.swift
+//  slot_machine_group16
+//
+//  Created by Yachao on 2023-01-22.
+//
+
+import SwiftUI
+
+@main
+struct slot_machine_group16App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
